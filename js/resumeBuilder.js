2b7a6c658500d32bc3aa261553c079ff05b3be43
sugarcookie <latasha.hussey@gmail.com>
2017-01-25 17:35:16 -0600

    Add social media contacts to bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,19 +4,26 @@
 var bio = {
     "name": "LaTasha Hussey",
     "role": "Front-End Web Developer",
-    "phone": "713-487-7391",
-    "email": "latasha.hussey@gmail.com",
-    "pictureURL": "images/tasha-tilt.jpg",
+    "contacts": {
+        "mobile": "713-487-7391",
+        "email": "latasha.hussey@gmail.com",
+        "github":"latashahussey",
+        "twitter":"@latashahussey",
+        "location":"Austin"
+    },
     "welcomeMessage": "Hello",
-    "skills":"HTML5, CSS3, JavaScript, jQuery, Git, Gulp, WordPress"
+    "skills": [
+        "HTML5", "CSS3", "JavaScript", "jQuery", "Git", "Gulp", "WordPress"
+    ],
+    "bioPic": "images/tasha-tilt.jpg"
 };
 
 /* Replace header and role */
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMobile = HTMLmobile.replace("%data%", bio.phone);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
