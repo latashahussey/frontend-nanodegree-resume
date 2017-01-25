5e7afcc5e1693fbdc55e02984f9cbe93c4666ed9
sugarcookie <latasha.hussey@gmail.com>
2017-01-25 17:15:03 -0600

    Create biography object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+/* Bio */
+var bio = {
+    "name": "LaTasha Hussey",
+    "role": "Front-End Web Developer",
+    "phone": "713-487-7391",
+    "email": "latasha.hussey@gmail.com",
+    "pictureURL": "images/tasha-tilt.jpg",
+    "welcomeMessage": "Hello",
+    "skills":"HTML5, CSS3, JavaScript, jQuery, Git, Gulp, WordPress"
+};
+
+/* Replace header and role */
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.phone);
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+
+/* Add bio to header */
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedBioPic);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
+$("#header").append(formattedSkills);
+$("#header").append(formattedWelcomeMsg);
